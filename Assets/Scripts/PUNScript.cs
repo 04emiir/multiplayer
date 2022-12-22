89ddf8c72d1afea9a28352dcd44c44c6d65d4eea
04emiir <emilio.nero3@gmail.com>
2022-12-22 12:45:22 +0100

    sdfs

---
+++ Assets/Scripts/PUNScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PUNScript : MonoBehaviourPunCallbacks {
+    public TMP_InputField usernameInput;
+    public TextMeshProUGUI buttonText;
+    // Start is called before the first frame update
+    public void OnClickConnect() {
+        PhotonNetwork.NickName = usernameInput.text;
+        buttonText.fontSize = 56;
+        buttonText.text = "Conectando...";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        SceneManager.LoadScene("Lobby");
+    }
+}
+
+
